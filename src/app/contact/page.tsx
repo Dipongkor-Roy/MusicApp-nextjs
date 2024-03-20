4fe5c9177f0ea82b13530a9d96b7522399d78668
Dipongkor-Roy <juniordipu@gmail.com>
2024-03-20 23:57:04 +0600

    done this project

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,7 +21,7 @@ const page = () => {
         />
         <textarea
           placeholder="  Write Your FeedBack....."
-          rows={"4"}
+          rows={4}
           className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
         />
         
